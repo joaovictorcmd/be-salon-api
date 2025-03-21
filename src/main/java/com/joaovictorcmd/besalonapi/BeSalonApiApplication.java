7376e574997b2909f730daf4f6b882b88168ddfd
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-03-21 11:02:31 -0300

    feat(project): start spring boot project

---
+++ src/main/java/com/joaovictorcmd/besalonapi/BeSalonApiApplication.java
@@ -0,0 +1,13 @@
+package com.joaovictorcmd.besalonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSalonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeSalonApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joaovictorcmd/besalonapi/BeSalonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovictorcmd.besalonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSalonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
