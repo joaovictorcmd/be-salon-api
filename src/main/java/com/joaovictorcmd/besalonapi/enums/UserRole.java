e796fe120c618ea7b5a3dd0fd7f559cebd0f9e69
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-03-21 22:48:48 -0300

    fix(user): standardize role naming for proper authorization

--- src/main/java/com/joaovictorcmd/besalonapi/enums/UserRole.java
+++ src/main/java/com/joaovictorcmd/besalonapi/enums/UserRole.java
@@ -1,19 +1,11 @@
 package com.joaovictorcmd.besalonapi.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 /**
  * @author joaovictorcmd
  * @date 2025 Mar 21
  */
-@AllArgsConstructor
-@Getter
 public enum UserRole {
-
-    ADMIN("ROLE_ADMIN"),
-    EMPLOYEE("ROLE_EMPLOYEE"),
-    CUSTOMER("ROLE_CUSTOMER");
-
-    private final String role;
+    ROLE_ADMIN,
+    ROLE_EMPLOYEE,
+    ROLE_CUSTOMER;
 }
